31ec641a1ead0b9788174a09efc5367b6bfa5365
rdbuss <robert.d.buss@gmail.com>
2016-04-30 13:25:50 -0700

    Added items to League and Inning dropdown menus

--- mainscreen.php
+++ mainscreen.php
@@ -22,27 +22,38 @@ NOTE: You do not have to fill them all out. <br />
 League:  <br />
 <select league = "lea">
 <option value  =""?> Select...</option>
+<option value  ="National"?>National</option>
+<option value  ="American"?>American</option>
 </select>
 </p>
 
 <p>
 PlayerID:  <br />
 <select playerID = "pID">
-<option value  =""?> Select...</option>
+<option value  =""?>Select...</option>
 </select>
 </p>
 
 <p>
 GameID: <br />
 <select gameID = "gID"> 
-<option value  =""?> Select...</option>
+<option value  =""?>Select...</option>
 </select>
 </p>
 
 <p>
 Inning: <br />
 <select inning = "inn"> <br />
-<option value  =""?> Select...</option>
+<option value  =""?>Select...</option>
+<option value  = "1"?>1</option>
+<option value  = "1"?>2</option>
+<option value  = "1"?>3</option>
+<option value  = "1"?>4</option>
+<option value  = "1"?>5</option>
+<option value  = "1"?>6</option>
+<option value  = "1"?>7</option>
+<option value  = "1"?>8</option>
+<option value  = "1"?>9</option>
 </select>
 </p>
 
