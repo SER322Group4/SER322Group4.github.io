68e27bc2cd7fab2bb6144b0a32dd32e5306c6bca
rdbuss <robert.d.buss@gmail.com>
2016-05-01 15:21:56 -0700

    Added completed resultsPage.php

--- resultsPage.php
+++ resultsPage.php
@@ -1,22 +1,56 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Results</title>
-</head>
-<body>
 <?php
 session_start();
-$selectedInning = $_POST['inning'];
-$selectedGame = $_POST['gameID'];
-$selectedPlayer = $_SESSION['pID'];
-echo "Game: " . $selectedGame;
-echo "</br>";
-echo "Inning: " . $selectedInning;
-echo "</br>";
-echo "Player ID: " . $selectedPlayer;
 ?>
-<br>
-<a href="mainscreen.php">Back to main page</a>
-</body>
-</html>
-<!--SELECT * FROM seasonStats WHERE playerID = $selectedPlayer AND gameID =$selectedGame AND inning = $selectedInning;-->
+<form method = "post" action = "resultsPage.php">
+<?php
+$selectedPlayer = $_SESSION['pID'];
+$selectedGame = $_POST['gameID'];
+$selectedInning = $_POST['inning'];
+mysql_connect('localhost','root','password');
+mysql_select_db('Baseball');
+
+$sql = "SELECT * FROM seasonStats WHERE playerID = $selectedPlayer AND gameID = $selectedGame AND inning = $selectedInning;";
+$result = mysql_query($sql);
+
+$statTitleInfo = "SELECT firstName, lastName FROM players WHERE playerID = $selectedPlayer;";
+$titleInfo = mysql_query($statTitleInfo);
+
+echo "<p align=left>Results for ";
+
+while($row = mysql_fetch_array($titleInfo)){
+	echo $row['firstName'] . " " .  $row['lastName'] . " in Game " . $selectedGame . ", Inning " . $selectedInning . ".</p>";
+}
+
+echo '<table align="left"
+cellspacing="5" cellpadding="8">
+ 
+<tr><td align="left"><b>atBats</b></td>
+<td align="left"><b>hits</b></td>
+<td align="left"><b>homeruns</b></td>
+<td align="left"><b>rbi</b></td>
+<td align="left"><b>steals</b></td>
+<td align="left"><b>errors</b></td>
+<td align="left"><b>pitOuts</b></td>
+<td align="left"><b>pitKs</b></td>
+<td align="left"><b>pitHits</b></td>
+<td align="left"><b>pitWalks</b></td>
+<td align="left"><b>pitER</b></td></tr>';
+
+while($row = mysql_fetch_array($result)){
+	echo '<tr><td align="left">' .
+	$row['atBats'] . '</td><td align="left">' .
+	$row['hits'] . '</td><td align="left">' .
+	$row['homeruns'] . '</td><td align="left">' .
+	$row['rbi'] . '</td><td align="left">' .
+	$row['steals'] . '</td><td align="left">' .
+	$row['errors'] . '</td><td align="left">' .
+	$row['pitOuts'] . '</td><td align="left">' .
+	$row['pitKs'] . '</td><td align="left">' .
+	$row['pitHits'] . '</td><td align="left">' .
+	$row['pitWalks'] . '</td><td align="left">' .
+$row['pitER'] . '</td>';
+
+echo '</tr>';
+}
+
+echo '</table>';
